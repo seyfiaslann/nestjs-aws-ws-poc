6bd5875b77c392a364ecc0c2969f8022e4ce13a1
seyfiaslann <seyfi.aslan@brandbastion.com>
2019-08-20 18:26:56 +0300

    Added return

--- handlers/api-ui-ws-listener.ts
+++ handlers/api-ui-ws-listener.ts
@@ -1,10 +1,17 @@
 import { Context, Handler } from 'aws-lambda';
+import { ApiGatewayManagementApi } from 'aws-sdk';
 
-export const handler: Handler = async (event: any, context: Context, callback: any) => {
+export const handler: Handler = async (event: any, context: Context) => {
     console.log(JSON.stringify(event));
 
-    callback(null, {
-        statusCode: 200,
-        body: 'Hello',
+    const apigwManagementApi = new ApiGatewayManagementApi({
+        apiVersion: '2018-11-29',
+        endpoint: event.requestContext.domainName + '/' + event.requestContext.stage,
     });
+
+    const connectionId = event.requestContext.connectionId;
+
+    await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: { body: 'hello' } }).promise();
+
+    return { body: 'Hello' };
 };
