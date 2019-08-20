efe169aa78913026601534b64bbd07029953b1bb
seyfiaslann <seyfi.aslan@brandbastion.com>
2019-08-20 15:05:17 +0000

    Working version

--- handlers/api-ui-ws-listener.ts
+++ handlers/api-ui-ws-listener.ts
@@ -1,6 +1,10 @@
 import { Context, Handler } from 'aws-lambda';
 
-export const handler: Handler = async (event: any, context: Context) => {
-  console.log(JSON.stringify(event));
-  return { body: 'Hello' };
+export const handler: Handler = async (event: any, context: Context, callback: any) => {
+    console.log(JSON.stringify(event));
+
+    callback(null, {
+        statusCode: 200,
+        body: 'Hello',
+    });
 };
